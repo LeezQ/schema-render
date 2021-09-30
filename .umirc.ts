dbdf7adee5a85c2d2f0cf3445ef509b50e73ddc3
guimian <guimian@jushuitan.com>
2021-09-30 14:59:50 +0800

    update: site config

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  title: 'schema-components',
+  title: 'schema-render',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo: '/images/logo2.png',
   outputPath: 'docs-dist',
   extraBabelPlugins: [
     [
